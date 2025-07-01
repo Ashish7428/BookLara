e64de74f0aec929de9e89a7b5feed845b1719d68
Ashish7428 <92727812+Ashish7428@users.noreply.github.com>
2025-07-01 07:12:08 +0530

    admin table

--- database/migrations/2024_03_18_000000_create_admins_table.php
+++ database/migrations/2024_03_18_000000_create_admins_table.php
@@ -10,21 +10,23 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('admins', function (Blueprint $table) {
-            $table->id();
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('admins')) {
+            Schema::create('admins', function (Blueprint $table) {
+                $table->id();
+                $table->string('username')->unique();
+                $table->string('password');
+                $table->rememberToken();
+                $table->timestamps();
+            });
 
-        // Insert default admin
-        DB::table('admins')->insert([
-            'username' => 'admin123',
-            'password' => Hash::make('admin@123'),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+            // Insert default admin
+            DB::table('admins')->insert([
+                'username' => 'admin123',
+                'password' => Hash::make('admin@123'),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
     }
 
     public function down()
