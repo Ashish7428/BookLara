f6821e697f928d069f3e74ca9b6074ab467bf2a0
Ashish7428 <92727812+Ashish7428@users.noreply.github.com>
2025-07-01 07:35:58 +0530

    session table

--- database/migrations/2025_03_18_171833_create_users_table.php
+++ database/migrations/2025_03_18_171833_create_users_table.php
@@ -8,19 +8,21 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('gender');
-            $table->integer('age');
-            $table->text('bio')->nullable();
-            $table->string('password');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
-            $table->timestamps();
-        });
+         if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('first_name');
+                $table->string('last_name');
+                $table->string('email')->unique();
+                $table->string('gender');
+                $table->integer('age');
+                $table->text('bio')->nullable();
+                $table->string('password');
+                $table->timestamp('email_verified_at')->nullable();
+                $table->rememberToken();
+                $table->timestamps();
+            });
+        }
     }
 
     public function down()
